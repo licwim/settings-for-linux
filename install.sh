90342f14c806dc774684d1c853c80c0e76201ecc
Krona Dune <kdune@mi-h4.21-school.ru>
2019-09-30 23:38:48 +0300

    сууууууук

--- install.sh
+++ install.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+
+if dpkg --get-selections|grep 'install'|grep 'sudo' 1>/dev/null
+then
+echo
+else
+echo "Please, install 'sudo'."
+exit
+fi
+
+if sudo -l | grep "may not run sudo"
+then
+echo "Please, add the user to sudoers."
+exit
+fi
+
 sudo apt-get update
+sudo apt-get install git -y
+sudo apt-get install vim -y
+sudo apt-get 
